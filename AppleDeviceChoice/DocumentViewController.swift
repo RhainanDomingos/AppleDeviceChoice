6ef7e9021359f2fce1f7e2b092ae256e9c29fe33
Rhainan Domingos <rhainandomingos@icloud.com>
2025-04-10 17:47:06 -0300

    Initial Commit

---
+++ AppleDeviceChoice/DocumentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DocumentViewController.swift
+//  AppleDeviceChoice
+//
+//  Created by Rhainan Domingos on 10/04/25.
+//
+
+import UIKit
+
+class DocumentViewController: UIDocumentViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
